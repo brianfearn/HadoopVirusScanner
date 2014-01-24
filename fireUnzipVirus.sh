fa92a3f36748d403eef60c573731a51fcf1d2d0c
Elijah Wright <elijah_wright@yahoo.com>
2014-01-24 14:23:53 -0500

    Brians Script file

--- fireUnzipVirus.sh
+++ fireUnzipVirus.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 set x = 0;
 for Z_FILE in *.zip; do
     let x++;
@@ -9,8 +8,4 @@ for Z_FILE in *.zip; do
     echo $x.exe;
     xxd $x.exe $x.hexdump;
 done;
-=======
-for Z_FILE in *.zip; do unzip -P infected $Z_FILE; done;
-set count = 0;
-for Z_FILE in *.pl; do xxd $Z_FILE $Z_FILE.hexdump; set count += 1; echo count; done;
->>>>>>> origin/master
+
