4b1ad604b5f872ea71abf8b146011777be8bbc17
Sean Smith <tetsutechnology@gmail.com>
2014-02-22 02:59:16 -0500

    clean up moved files

--- fireUnzipVirus.sh
+++
@@ -1,7 +0,0 @@
-for name in *.zip; do
-    unzip -P infected $name;
-    name="${name%.*}"
-    mv malware.exe $name.exe;
-    xxd $name.exe $name.exe.hexdump;
-    echo "Created $name.exe and $name.hexdump"
-done;
