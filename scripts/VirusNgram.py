191e4152547b78604c7ecac6482588246c5adaea
Tetsutechnology <tetsutechnology@gmail.com>
2014-02-27 17:55:15 -0500

    more changes to ngram script

--- scripts/VirusNgram.py
+++ scripts/VirusNgram.py
@@ -7,8 +7,8 @@ import shutil
 
 os.chdir('./../data/virus/')
 virus_loc = os.getcwd()
-os.chdir('./../normal/')
-normal_loc = os.getcwd()
+#os.chdir('./../normal/')
+#normal_loc = os.getcwd()
 os.chdir('./../')
 
 if os.path.exists(os.getcwd() + '/output/'):
@@ -19,32 +19,51 @@ else:
 	os.chdir('./output/')
 	print os.getcwd()
 
+os.mkdir(os.getcwd() + '/output/virus/')
+os.mkdir(os.getcwd() + '/output/normal/')
+
 ngram_buffer = []
 
 v = path(virus_loc)
 v_files = v.walkfiles()
 
-n = path(normal_loc)
-n_files = n.walkfiles()
+#n = path(normal_loc)
+#n_files = n.walkfiles()
+
+
+normal_features = set()
 
 for f in v_files:
 	
 	with open(f, 'rb') as f2:
 		
 		output_name = f.name
+		with open(os.getcwd() + '/output/virus/' + f.name + '.bif', 'a') as f3:
 
-		while 1:
-			byte = f2.read(1)
-			if not byte:
-				break
-			if len(ngram_buffer) == 7:
+			virus_features = set()
+
+			while 1:
+				byte = f2.read(1)
+				if not byte:
+					break
+				if len(ngram_buffer) == 7:
 				#write out 7 byte feature
-				print ''.join(ngram_buffer)
-				ngram_buffer.pop(0)
-				ngram_buffer.append(binascii.hexlify(byte))
-			else:
-				ngram_buffer.append(binascii.hexlify(byte))
+					feature = (''.join(ngram_buffer))
+					if feature not in virus_features:
+						virus_features.add(feature)
 
+
+					#f3.write(feature)
+					#f3.write('\n')
+					ngram_buffer.pop(0)
+					ngram_buffer.append(binascii.hexlify(byte))
+				else:
+					ngram_buffer.append(binascii.hexlify(byte))
+
+			for vf in virus_features:
+				f3.write('+')
+				f3.write(vf)
+				f3.write('\n')
 
 
 		
